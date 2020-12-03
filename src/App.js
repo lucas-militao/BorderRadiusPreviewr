42427624269059008f7f1925876c527aed1f541f
lumilitas <lucas23121995>
2020-12-03 19:42:54 -0300

    copying css genrated text to clipboard

--- src/App.js
+++ src/App.js
@@ -13,22 +13,77 @@ export default function App() {
   const [ height, setHeight ] = useState('');
   const [ width, setWidth ] = useState('');
 
+  const [ css, setCss ] = useState(`
+    .rectangle {
+      border-radius: ${topLeftRadiusValue} ${topRightRadiusValue} ${bottomLeftRadiusValue} ${bottomRightRadiusValue};
+      height: ${height};
+      width: ${width};
+    }
+  `);
+
   const [ divRectangle, setDivRectangle ] = useState({});
 
   function handleBorderRadius() {
     setDivRectangle({
       height: `${height}`,
       width: `${width}`,
-      borderRadius: `${topLeftRadiusValue}px ${topRightRadiusValue}px ${bottomLeftRadiusValue}px ${bottomRightRadiusValue}px`
+      borderRadius: `${topLeftRadiusValue} ${topRightRadiusValue} ${bottomLeftRadiusValue} ${bottomRightRadiusValue}`
     })
+
+    if(height == '' || height == '0px') {
+      setCss(`
+        .rectangle {
+          border-radius: ${topLeftRadiusValue} ${topRightRadiusValue} ${bottomLeftRadiusValue} ${bottomRightRadiusValue};
+        }
+      `)
+    } else {
+      setCss(`
+        .rectangle {
+          border-radius: ${topLeftRadiusValue} ${topRightRadiusValue} ${bottomLeftRadiusValue} ${bottomRightRadiusValue};
+          height: ${height};
+          width: ${width};
+        }
+      `)
+    }
   }
 
   function handleSize() {
     setDivRectangle({
       height: `${height}`,
       width: `${width}`,
-      borderRadius: `${topLeftRadiusValue}px ${topRightRadiusValue}px ${bottomLeftRadiusValue}px ${bottomRightRadiusValue}px`
+      borderRadius: `${topLeftRadiusValue} ${topRightRadiusValue} ${bottomLeftRadiusValue} ${bottomRightRadiusValue}`
     })
+
+    if(topLeftRadiusValue == '' && topLeftRadiusValue == '0px') {
+      setCss(`
+        .rectangle {
+          height: ${height};
+          width: ${width};
+        }
+      `)
+    } else {
+      setCss(`
+        .rectangle {
+          border-radius: ${topLeftRadiusValue} ${topRightRadiusValue} ${bottomLeftRadiusValue} ${bottomRightRadiusValue};
+          height: ${height};
+          width: ${width};
+        }
+      `)
+    }
+  }
+
+  function handleCopyCss() {
+    var copyText = document.getElementById("cssText");
+
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
   }
 
   return (
@@ -48,10 +103,10 @@ export default function App() {
           <Form.Label id="radiusLabel">Raio desejado</Form.Label>
 
           <div className="borderRadiusInputs">
-            <Form.Control id="topLeftRadius" type="text" value={topLeftRadiusValue} onChange={ event => setTopLeftRadiusValue(event.target.value) }/>
-            <Form.Control id="topRightRadius" type="text" value={topRightRadiusValue} onChange={ event => setTopRightRadiusValue(event.target.value) }/>
-            <Form.Control id="bottomLeftRadius" type="text" value={bottomLeftRadiusValue} onChange={ event => setBottomLeftRadiusValue(event.target.value) }/>
-            <Form.Control id="bottomRightRadius" type="text" value={bottomRightRadiusValue} onChange={ event => setBottomRightRadiusValue(event.target.value) }/>
+            <Form.Control id="topLeftRadius" type="text" value={topLeftRadiusValue} onChange={ event => setTopLeftRadiusValue(event.target.value) } placeholder='0px'/>
+            <Form.Control id="topRightRadius" type="text" value={topRightRadiusValue} onChange={ event => setTopRightRadiusValue(event.target.value) } placeholder='0px'/>
+            <Form.Control id="bottomLeftRadius" type="text" value={bottomLeftRadiusValue} onChange={ event => setBottomLeftRadiusValue(event.target.value) } placeholder='0px'/>
+            <Form.Control id="bottomRightRadius" type="text" value={bottomRightRadiusValue} onChange={ event => setBottomRightRadiusValue(event.target.value) } placeholder='0px'/>
           </div>
           
           <Button type="submit">Mudar Raio</Button>
@@ -67,12 +122,19 @@ export default function App() {
           <Form.Label>Tamanho do Retangulo</Form.Label>
 
           <div className="sizeInputs">
-            <Form.Control type="text" value={height} onChange={ event => setHeight(event.target.value) } placeholder="altura"/>
-            <Form.Control type="text" value={width} onChange={ event => setWidth(event.target.value) } placeholder="largura"/>
+            <label className="sizeLabels">Altura</label>
+            <Form.Control type="text" value={height} onChange={ event => setHeight(event.target.value) } placeholder="0px"/>
+            <label className="sizeLabels">Largura</label>
+            <Form.Control type="text" value={width} onChange={ event => setWidth(event.target.value) } placeholder="0px"/>
           </div>
 
           <Button type="submit">Mudar Tamanho</Button>
         </Form>
+
+        <div className="cssContainer">
+          <textarea id="cssText" cols="30" rows="10" value={css}/>
+          <button onClick={handleCopyCss}>Copiar</button>
+        </div>
       </div>
     </>
   )
